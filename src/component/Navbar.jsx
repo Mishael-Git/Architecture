ab9611e4b881354fcbda1559650ab3c570628728
Mishael <mishaelclifford1@gmail.com>
2023-03-22 12:45:47 +0100

    First commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { ImHome3 } from "react-icons/im";
 function Navbar() {
   return (
-    <div className="flex flex-row  sm:flex-col md:flex-row lg:flex-row  xl:flex-row 2xl:flex-row items-center justify-between px-5">
-      <div className="flex flex-row sm:flex-col md:flex-row  lg:flex-row xl:flex-row 2xl:flex-row items-center md:space-x-3 sm:space-y-3">
+    <div className="flex  flex-col md:flex-row items-center justify-between px-5">
+      <div className="flex flex-col  md:flex-row   items-center md:space-x-3 sm:space-y-3">
         <div className="bg-[#226089] rounded-full px-3 py-3">
-          <ImHome3 size={30} className="text-white"/>
+          <ImHome3 size={30} className="text-white" />
         </div>
         <h1 class="text-2xl text-[#226089]">ARCHITECTURE</h1>
       </div>
